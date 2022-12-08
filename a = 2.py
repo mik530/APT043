7c14e91cd9ee124774e9eb25bb08e1271a2ae355
mik530 <109447435+mik530@users.noreply.github.com>
2022-12-08 17:01:11 +0800

    Add files via upload

---
+++ a = 2.py
@@ -0,0 +1,30 @@
+'''a = 2
+print(type(a))
+
+
+'''
+'''a = 12
+b = 15
+c = a*b/a
+print(c)'''
+#a = input("a的值")
+#b = input("b的值")
+#c = a + b
+#print(c)
+'''dict['d':14,'g':18]
+a = input("$:")
+b = input("$:")
+c = a + b 
+if c = dict :
+  print("you are right")
+else:
+    print("feiled")
+    '''
+'''
+import requests
+x = requests.get('https://www.abidu.com')
+print(x.status_code)
+print(x.reason)
+'''
+
+
---
+++ s = 2.py
@@ -0,0 +1,20 @@
+'''s = 2
+print("s =", s )
+'''
+'''a = 12
+print("a =",a)
+print("a:",type(a))
+'''
+#a = int (input("第一个数"))
+#b = int (input("还有一个数"))
+#c=a*b
+#
+#print(c)
+'''
+a = 1
+a = 2
+a = 3
+print(a)
+'''
+f=input()
+print(f)
